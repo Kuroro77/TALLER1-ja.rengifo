f8e1b3adc40c3743390654743345d7da177eaf4f
Jhon Rengifo <jrengifo@famisanar.com.co>
2025-03-06 20:39:44 -0500

    Creacion modelo usuarios conectado a bd

---
+++ models/usuarios.py
@@ -0,0 +1,6 @@
+from config.db import db
+
+class Usuarios(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(16), nullable=False)
+    password = db.Column(db.String(16), nullable=False)
