be0eccffe4794a65f593c4e44b7d9bb5b98b1672
Jhon Rengifo <jrengifo@famisanar.com.co>
2025-03-09 11:00:25 -0500

    Funcionalidad login, redireccionamiento a bienvenida o pagina error

--- config/config.py
+++ config/config.py
@@ -1,7 +1,8 @@
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv(override=True)
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SECRET_KEY = 'ZOPVNT90W26'
--- controllers/home_controller.py
+++ controllers/home_controller.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from flask_login import login_user, current_user
+
 from models.usuarios import Usuarios
 from config.auth import login_manager
 
@@ -11,3 +13,16 @@ def load_user(user_id:int):
 @home_blueprint.route('/')
 def login():
     return render_template('login.html')
+
+@home_blueprint.route('/auth')
+def auth():
+    username = request.args.get("username")
+    password = request.args.get("password")
+
+    user = Usuarios.query.filter_by(username=username, password=password).first()
+
+    if user:
+        login_user(user)
+        return render_template('bienvenida.html', current_user= current_user)
+    else:
+        return render_template('404.html')
